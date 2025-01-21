043a14d2d2c2db875618767d4826fceac1b9e50e
Sree-harshitha12 <116336581+Sree-harshitha12@users.noreply.github.com>
2025-01-21 11:52:57 +0530

    Update adminmail.py

--- adminmail.py
+++ adminmail.py
@@ -3,7 +3,7 @@ from smtplib import SMTP
 from email.message import EmailMessage
 def adminsendmail(to,subject,body):
     server=smtplib.SMTP_SSL('smtp.gmail.com',465)
-    server.login('sreeharshitha46@gmail.com','ggcy rvji ekfs ygsh')
+    server.login('sreeharshitha46@gmail.com','saih kbme xmrm mahu')
     msg=EmailMessage()
     msg['From']='sreeharshitha46@gmail.com'
     msg['Subject']=subject
